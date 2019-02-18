4c266277ef214861ce00aacdb3a5f9bb77c4b7b2
Sivaprakash <sivpraka@cisco.com>
2019-02-18 15:34:43 +0530

    Using 3rd party library
    inthis we used _.isString()

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 console.log("Starting app-notes.js");
 
-const notes=require('./notes.js');
+//const notes=require('./notes.js');
+const _ = require('lodash');
 
-console.log("Age:"+notes.age);
-var res=notes.addNote();
-console.log("notes.addNote:"+res);
+console.log(_.isString('MyName'));
+console.log(_.isString(true));
+//console.log("Age:"+notes.age);
+//var res=notes.addNote();
+//console.log("notes.addNote:"+res);
 
-console.log("notes.add:"+notes.add(4,5));
+//console.log("notes.add:"+notes.add(4,5));
