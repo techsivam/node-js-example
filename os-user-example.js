9e153a436d5353873ff6e53e358943d2436d424b
Sivaprakash <sivpraka@cisco.com>
2019-02-18 13:58:47 +0530

    Update os-user-example.js
    getting value from os  variavle

--- os-user-example.js
+++ os-user-example.js
@@ -4,3 +4,8 @@ const fs = require('fs');
 const os =require('os');
 var user=os.userInfo();
 console.log(user);
+
+fs.appendFile('greetings.txt','Hello '+user.username, function (err){
+  if(err)
+  console.log('Unable to Write file');
+});
