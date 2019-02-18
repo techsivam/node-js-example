4e2d8f129afebec4aaa656c531d4fba8c7fec575
Sivaprakash <sivpraka@cisco.com>
2019-02-18 15:38:34 +0530

    Update app.js
    use uniq to remove duplicate values in array

--- app.js
+++ app.js
@@ -10,3 +10,8 @@ console.log(_.isString(true));
 //console.log("notes.addNote:"+res);
 
 //console.log("notes.add:"+notes.add(4,5));
+
+
+var fileteredArray=_.uniq(['Siva',1,'Siva',1,2,3,4]);
+
+console.log(fileteredArray);
